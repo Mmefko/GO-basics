bc42de6214ee8cb122f6a747cd1f2ff14189980a
Mikhail_Vasilyeu@epam.com <Mikhail_Vasilyeu@epam.com>
2023-01-16 17:51:42 +0100

    03 Math

--- 03 math/start/Basic/Basic_start.go
+++ 03 math/start/Basic/Basic_start.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//Print PI
+	fmt.Println(math.Pi) //3.141592653589793
+
+	// Ceiling and Floor Functions
+	fmt.Println(math.Ceil(math.Pi))  //4
+	fmt.Println(math.Floor(math.Pi)) //3
+
+	//Truncate will return int value if X
+	fmt.Printf("%.2f\n", math.Trunc(math.Pi)) //3.00
+
+	//Min and Max
+	fmt.Println(math.Max(math.Pi, math.Ln2)) //3.141592653589793
+	fmt.Println(math.Min(math.Pi, math.Ln2)) //0.6931471805599453
+
+	//Mod operator is like % but for float
+	fmt.Println(17 % 5)              //2
+	fmt.Println(math.Mod(17.0, 5.0)) //2
+
+	//Round and RoundToEven
+	fmt.Printf("%.1f\n", math.Round(10.5))  //11.0
+	fmt.Printf("%.1f\n", math.Round(-10.5)) //-11.0
+
+	fmt.Printf("%.1f\n", math.RoundToEven(10.5)) //10.0
+	fmt.Printf("%.1f\n", math.RoundToEven(11.5)) //12.0
+}
--- 03 math/start/Calculations/Calculations_start.go
+++ 03 math/start/Calculations/Calculations_start.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	x := 10.0
+
+	//Absolute value
+	fmt.Println(math.Abs(x), math.Abs(-x)) //10 10
+
+	//Pow(x^y) and exp(e^x)
+	fmt.Println(math.Pow(x, 2.0))    //100
+	fmt.Println(math.E, math.Exp(x)) //2.718281828459045 22026.465794806718
+
+	//Trigonometry and othr functions
+	fmt.Println(math.Cos(math.Pi))     //-1
+	fmt.Println(math.Sin(math.Pi / 2)) //1
+	fmt.Println(math.Tan(math.Pi))     //-1.2246467991473515e-16
+	fmt.Println(math.Log(10))          //2.302585092994046
+
+	//Square and cube roots
+	fmt.Println(math.Sqrt(25))  //5
+	fmt.Println(math.Cbrt(125)) //5
+
+	//Calculate the Hypothenuse of a right triangle
+	fmt.Println(math.Hypot(30, 40)) //50
+}
