391877f5ef244a915772d28a61b08a6bbe44bc16
Mikhail_Vasilyeu@epam.com <Mikhail_Vasilyeu@epam.com>
2023-01-18 18:07:11 +0100

    01 math / random

--- 03 math/start/Random/Random_start.go
+++ 03 math/start/Random/Random_start.go
@@ -1,3 +1,45 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	//Initialize the random seed to an unknown value
+	rand.Seed(time.Now().UnixNano())
+
+	//Generate random integer numbers
+	fmt.Println(rand.Int())    //4860731531654632520
+	fmt.Println(rand.Intn(10)) //2
+
+	//Generate random floating Point numbers
+	fmt.Println(rand.Float32()) //0.12687156
+	fmt.Println(rand.Float64()) //0.4985925691466604
+
+	//use the Perm function to create permutations
+	s := []string{"apple", "pear", "grape", "orange", "kiwi", "melon"}
+	indexes := rand.Perm(len(s))
+	fmt.Println(indexes) // 0 5 4 3 1 2]
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%v - ", s[indexes[i]]) //apple - melon - kiwi - orange - pear - grape
+	}
+	fmt.Println()
+
+	//generate a random integer between a and b
+	const a, b = 10, 50
+	for i := 0; i < 10; i++ {
+		n := a + rand.Intn(b-a+1)
+		fmt.Print(" ", n) // 15 36 46 46 41 24 16 47 47
+	}
+	fmt.Println()
+
+	//Generate a random uppercase character
+	for i := 0; i < 10; i++ {
+		c := string('A' + rune(rand.Intn('Z'-'A'+1)))
+		fmt.Print(" ", c) //  U S P Q Z W K U V A
+	}
+	fmt.Println()
+}
--- 03 math/start/UseRand/UseRand_start.go
+++ 03 math/start/UseRand/UseRand_start.go
@@ -1,3 +1,54 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func main() {
+	//Seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
+	//Shuffle a sequence of values
+	const numstring = "one two three four five six"
+	words := strings.Fields(numstring)
+	rand.Shuffle(len(words), func(i, j int) {
+		words[i], words[j] = words[j], words[i]
+	})
+
+	fmt.Println(words) //[six five three four one two]
+
+	//Generate a rendom string
+	const upletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	const lowletters = "abcdefghijklmnopqrstuvwxyz"
+	const digits = "0123456789"
+	const specialchars = "~=+%^*()[]{}!@#$?|"
+	const allchars = upletters + lowletters + digits + specialchars
+
+	var sb strings.Builder
+	length := 10
+
+	for i := 0; i < length; i++ {
+		sb.WriteRune(rune(allchars[rand.Intn(len(allchars))]))
+	}
+
+	fmt.Println("String result: ", sb.String()) //String result:  nL|7WML(1V
+
+	//Generate a random string with rules
+	buf := make([]byte, length)
+	buf[0] = digits[rand.Intn(len(digits))]
+	buf[1] = specialchars[rand.Intn(len(specialchars))]
+	buf[2] = upletters[rand.Intn(len(upletters))]
+	buf[3] = lowletters[rand.Intn(len(lowletters))]
+	for i := 4; i < length; i++ {
+		buf[i] = allchars[rand.Intn(len(allchars))]
+	}
+
+	rand.Shuffle(len(buf), func(i, j int) {
+		buf[i], buf[j] = buf[j], buf[i]
+	})
+
+	fmt.Println(string(buf)) //p@5yUb!A|H
+}
