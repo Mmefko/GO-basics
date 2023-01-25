b27c5c50eae1e199512a457edec36778883f54ff
Mikhail_Vasilyeu@epam.com <Mikhail_Vasilyeu@epam.com>
2023-01-25 18:08:08 +0100

    04 files

--- 04 files/start/Directories/directories_start.go
+++ 04 files/start/Directories/directories_start.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	//Create a directory
+	os.Mkdir("newdir", os.ModePerm) //new directory created ./newdir
+
+	//Create a deep directory
+	os.MkdirAll("path/to", os.ModePerm) //new directory created ./path/to
+
+	//Remove will remove an item
+	defer os.Remove("newdir")
+
+	//RemoveAll will remove an item and all children
+	defer os.RemoveAll("path")
+
+	//Get the current working directory
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Printf("Current dir is %v\n", dir) //Current dir is /Users/....
+
+	//Get the dirrectory of the curent process
+	procdir, err := os.Executable()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Printf("Current dir for the process is %v\n", procdir) //Current dir for the process is /var/folders/...
+
+	//Read the contents of a directory
+	contents, _ := ioutil.ReadDir(".")
+	for _, fi := range contents {
+		fmt.Printf("Name is %v : is dir = %v\n", fi.Name(), fi.IsDir())
+	}
+}
--- 04 files/start/Tempfiles/tempfiles_start.go
+++ 04 files/start/Tempfiles/tempfiles_start.go
@@ -1,3 +1,37 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	//Get the default temporary directory path
+	tempPath := os.TempDir()
+	fmt.Printf("Default temp path is %v\n", tempPath) //Default temp path is /var/folders/75/7xt06bbs24d2vkmz88vn4xmc0000gp/T/
+
+	//Create a temporary file use TempFile
+	tempContent := []byte("Some text")
+	tmpFile, err := ioutil.TempFile(tempPath, "tempfile_*.txt")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if _, err := tmpFile.Write(tempContent); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	//Read and Print the tempFile content
+	data, _ := ioutil.ReadFile(tmpFile.Name())
+	fmt.Printf("%s\n", data) //Some text
+
+	//Remove the tempfile when finished
+	fmt.Printf("tmpFile name: %v\n", tmpFile.Name()) //tmpFile name: /var/folders/75/7xt06bbs24d2vkmz88vn4xmc0000gp/T/tempfile_2932882046.txt
+	defer os.Remove(tmpFile.Name())
+
+	//Creae a temporary directory with ioutil's version of TempDir
+	tmpDir, _ := ioutil.TempDir("", "tempdir*")
+	fmt.Printf("tmpDir name: %v\n", tmpDir) //tmpDir name: /var/folders/75/7xt06bbs24d2vkmz88vn4xmc0000gp/T/tempdir298616879
+	defer os.Remove(tmpDir)
+}
